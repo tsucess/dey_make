3ae983dc32183ca14fe747d4bb95621ec6c974c5
Abosede Oloude Oyamine <oloudeabosede26@gmail.com>
2026-03-27 11:20:48 +0100

    fixed category

--- src/pages/CreateUpload.jsx
+++ src/pages/CreateUpload.jsx
@@ -331,22 +331,11 @@ export default function CreateUpload() {
                 className="w-full rounded-2xl bg-white px-4 py-4 text-sm text-slate100 outline-none dark:bg-[#1F1F1F] dark:text-white"
               >
                 <option value="">Choose a category</option>
-                {categories.length > 1 ? categories.map((category) => (
+                { categories.map((category) => (
                   <option key={category.id} value={category.id}>
                     {category.label || category.name}
                   </option>
-                )) : <> 
-  <option value="technology">Technology</option>
-  <option value="health">Health & Wellness</option>
-  <option value="education">Education</option>
-  <option value="entertainment">Entertainment</option>
-  <option value="business">Business & Finance</option>
-  <option value="lifestyle">Lifestyle</option>
-  <option value="travel">Travel</option>
-  <option value="food">Food & Recipes</option>
-  <option value="fashion">Fashion & Beauty</option>
-  <option value="sports">Sports</option>
-</>}
+                )) }
               </select>
             </div>
           </div>
