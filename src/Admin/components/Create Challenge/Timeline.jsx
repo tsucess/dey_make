b5a800043f242223b02067cafa8e3f243728b19b
Abosede Oloude Oyamine <oloudeabosede26@gmail.com>
2026-08-04 14:07:32 +0100

    finshed timeline

--- src/Admin/components/Create Challenge/Timeline.jsx
+++ src/Admin/components/Create Challenge/Timeline.jsx
@@ -45,10 +45,36 @@ function Timeline() {
       isActive: true,
     },
   ]);
+  const [criteria, setCriteria] = useState([]);
+  const [addCriteria, setAddCriteria] = useState(false);
 
-  function handleToggleActive(title){
-   setRules(prev => prev.map(rule => rule.title === title ? {...rule, isActive : !rule.isActive} : rule))
+  function handleToggleActive(title) {
+    setRules((prev) =>
+      prev.map((rule) =>
+        rule.title === title ? { ...rule, isActive: !rule.isActive } : rule,
+      ),
+    );
   }
+
+  function handleAddCriteria() {
+    setAddCriteria((prev) => !prev);
+  }
+
+  function handleAddNewCriteria(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    setCriteria((prev) => [
+      ...prev,
+      {
+        title: formData.get("title"),
+        description: formData.get("description"),
+        percent: formData.get("percent"),
+      },
+    ]);
+    handleAddCriteria();
+  }
+
   return (
     <section className="flex flex-col gap-6">
       <div className="flex flex-col gap-1">
@@ -104,19 +130,98 @@ function Timeline() {
           </p>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-2.5 gap-y-4">
-            {
-                rules.map(rule => <div key={rule.title} className="flex flex-col gap-2 p-5 rounded-xl bg-blue500">
-                    <div className="flex items-center justify-between">
-                        <h5 className="text-sm font-bold text-white">{rule.title}</h5>
-                        <button onClick={()=> handleToggleActive(rule.title)} className={`w-5.5 h-3 cursor-pointer rounded-full p-0.5 items-center flex ${
-                            rule.isActive ? 'bg-orange100 justify-end' : 'justify-start border-2 border-slate250'
-                        }`}>
-                            <span className={`w-2 h-2 rounded-full ${rule.isActive ? 'bg-slate100' : 'bg-slate250'}`}></span>
-                        </button>
-                    </div>
-                    <p className="text-xs text-white">{rule.desc}</p>
-                </div> )
-            }
+          {rules.map((rule) => (
+            <div
+              key={rule.title}
+              className="flex flex-col gap-2 p-5 rounded-xl bg-blue500"
+            >
+              <div className="flex items-center justify-between">
+                <h5 className="text-sm font-bold text-white">{rule.title}</h5>
+                <button
+                  onClick={() => handleToggleActive(rule.title)}
+                  className={`w-5.5 h-3 cursor-pointer rounded-full p-0.5 items-center flex ${
+                    rule.isActive
+                      ? "bg-orange100 justify-end"
+                      : "justify-start border-2 border-slate250"
+                  }`}
+                >
+                  <span
+                    className={`w-2 h-2 rounded-full ${rule.isActive ? "bg-slate100" : "bg-slate250"}`}
+                  ></span>
+                </button>
+              </div>
+              <p className="text-xs text-white">{rule.desc}</p>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-1">
+            <h3 className="text-white text-lg font-medium">
+              Judging Criteria (Optional)
+            </h3>
+            <p className="text-sm text-white">
+              Set the criteria that will be used to evaluate entries.
+            </p>
+          </div>
+          <div className="flex items-center gap-3 flex-wrap">
+            {criteria.map((item) => (
+              <div className="border font-roboto border-white/30 w-50 h-32 rounded-md p-5 flex flex-col gap-2">
+                <h5 className="text-base text-white">{item.title}</h5>
+                <p className="text-xs text-zinc300">{item.description}</p>
+                <span className="text-[11px] text-zinc300">{item.percent}</span>
+              </div>
+            ))}
+            <div className="border border-orange100 border-dashed w-50 h-32 rounded-md flex items-center justify-center py-1 px-3">
+              {!addCriteria ? (
+                <button
+                  onClick={handleAddCriteria}
+                  className="font-roboto text-xs font-medium text-orange100"
+                >
+                  Add Criteria
+                </button>
+              ) : (
+                <form
+                  className="flex flex-col gap-1.5 w-full "
+                  onSubmit={handleAddNewCriteria}
+                >
+                  <input
+                    type="text"
+                    name="title"
+                    id=""
+                    placeholder="title"
+                    className="text-xs text-orange100 outline-none border-b border-orange100 w-full"
+                  />
+                  <input
+                    type="text"
+                    name="description"
+                    id=""
+                    placeholder="description"
+                    className="text-xs text-orange100 outline-none border-b border-orange100 w-full"
+                  />
+                  <input
+                    type="text"
+                    name="percent"
+                    id=""
+                    placeholder="percentage"
+                    className="text-xs text-orange100 outline-none border-b border-orange100 w-full"
+                  />
+                  <button className="font-roboto text-xs font-medium text-orange100">
+                    Add Criteria
+                  </button>
+                </form>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-between bg-blue500 rounded-xl py-6 px-5">
+        <div className="flex items-center gap-5 font-roboto">
+          <MdInfoOutline className="w-5 h-5 text-white" />
+          <span className="text-sm text-white">
+            Total must equal 100%. Curent total:{" "}
+            <span className="font-semibold">100%</span>
+          </span>
         </div>
       </div>
     </section>
